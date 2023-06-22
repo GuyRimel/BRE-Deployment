300e0fa41ccdf67ef41cfa30ae25f848707cafc5
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2023-06-22 14:15:11 -0500

    updated app name

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['calm-everglades-04413-fa92fd3b22a3.herokuapp.com','127.0.0.1', 'https://calm-everglades-04413-fa92fd3b22a3.herokuapp.com']
+ALLOWED_HOSTS = ['bestrecipesever-3d5fc87c55f3.herokuapp.com','127.0.0.1']
 
 
 
