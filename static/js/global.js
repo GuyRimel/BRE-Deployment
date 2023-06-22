32d813c70a0962d74a7406516d4aa2752063268f
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2023-06-22 17:31:28 -0500

    added images, updated js

--- static/js/global.js
+++ static/js/global.js
@@ -1,8 +1,9 @@
 docLink = document.createElement('div');
 docLink.innerHTML = '<a href="{% url "doc" %}" target="_blank">Psst! ... BRE Documentation</a>';
-docLink.style.position = 'absolute';
+docLink.style.position = 'sticky';
 docLink.style.right = 0;
 docLink.style.bottom = 0;
+docLink.style.width = 'max-content';
 docLink.style.backgroundColor = '#eaeaea';
 docLink.style.padding = '.4rem';
 docLink.style.border = '1px solid black';
